b3f2a9b2892415b40657eb5cf9790f30a618e92c
safinhr <hrsafin2434@gmail.com>
2024-01-09 18:25:59 +0600

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h2 className=" font-Poppins">Hello World</h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdP6vfplPo8KPY1gniK2pEtnr00i_liBM",
+  authDomain: "skillforge-70857.firebaseapp.com",
+  projectId: "skillforge-70857",
+  storageBucket: "skillforge-70857.appspot.com",
+  messagingSenderId: "10576026061",
+  appId: "1:10576026061:web:df05e7342d88e000288c84"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
