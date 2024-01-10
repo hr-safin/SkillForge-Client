43d840bdc8e5320919b6d616087719de1b34d87a
safinhr <hrsafin2434@gmail.com>
2024-01-10 11:09:45 +0600

    preloader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,48 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./AuthProvider/AuthProvider";
 import { MainLayout } from "./MainLayout/MainLayout.";
 import Login from "./Component/Login/Login";
 import SignUp from "./Component/SignUp/SignUp";
-
+import { useEffect, useState } from "react";
+import ScaleLoader from "react-spinners/ScaleLoader";
 function App() {
+  const [isLoading, setIsLoading] = useState("Loading");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
 
   const router = new createBrowserRouter([
     {
-      path : "/",
-      element :<MainLayout />,
-      errorElement : <Error />,
-      children : [
+      path: "/",
+      element: <MainLayout />,
+      errorElement: <Error />,
+      children: [
         {
-          path : "/login",
-          element : <Login ></Login>
+          path: "/login",
+          element: <Login></Login>,
         },
         {
-          path : "/signUp",
-          element : <SignUp />
-        }
-      ]
-    }
-  ])
+          path: "/signUp",
+          element: <SignUp />,
+        },
+      ],
+    },
+  ]);
 
-
-  return (
+  return isLoading ? (
+    <div className="flex text-3xl justify-center items-center h-screen">
+      <ScaleLoader color="#2563eb" />
+    </div>
+  ) : (
     <>
-      <AuthProvider >
+      <AuthProvider>
         <RouterProvider router={router}></RouterProvider>
       </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
