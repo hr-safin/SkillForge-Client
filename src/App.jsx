ca54c22f3f4cc7104e96698a78c83974d486f3a2
safinhr <hrsafin2434@gmail.com>
2024-01-11 20:50:02 +0600

    course section added

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import Course from "./Component/Home/Course/Course";
 
 
 const queryClient = new QueryClient()
@@ -36,6 +37,10 @@ function App() {
         {
           path : "/",
           element : <Home />
+        },
+        {
+          path : "/course",
+          element : <Course />
         },
         {
           path: "/login",
