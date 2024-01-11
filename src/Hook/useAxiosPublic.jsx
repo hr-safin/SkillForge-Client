87969ec48cae4cc777359b9dd4a1c8e599bbccdd
safinhr <hrsafin2434@gmail.com>
2024-01-11 19:08:04 +0600

    use custom hook for backend URL

---
+++ src/Hook/useAxiosPublic.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import axios from 'axios';
+
+const axiosSecure = axios.create({
+    baseURL : "http://localhost:5000"
+})
+
+
+const useAxiosPublic = () => {
+    return axiosSecure
+};
+
+export default useAxiosPublic;
