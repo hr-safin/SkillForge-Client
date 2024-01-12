a78047f9d17350bbcc8546a706c82987c1c3f133
safinhr <hrsafin2434@gmail.com>
2024-01-12 23:23:46 +0600

    course json data updated

--- src/Component/Home/Course/Enroll.jsx
+++ src/Component/Home/Course/Enroll.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import useAxiosPublic from '../../../Hook/useAxiosPublic';
 
 const Enroll = () => {
-
+    
+    window.scrollTo(0,0)
     const {id} = useParams()
-    console.log(id)
+    const axiosPublic = useAxiosPublic()
+    const [enroll, setEnroll] = useState([])
+
+    useEffect(() => {
+        axiosPublic.get(`/enroll/${id}`)
+        .then(res => {
+            console.log(res.data)
+            setEnroll(res.data)
+        })
+    }, [])
     return (
         <div>
             
