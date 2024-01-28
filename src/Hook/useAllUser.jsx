22361bd8fc8b3625c7e619f0222e4e6e85642535
safinhr <hrsafin2434@gmail.com>
2024-01-28 19:12:51 +0600

    fetch all the user in the client side

--- src/Hook/useAllUser.jsx
+++ src/Hook/useAllUser.jsx
@@ -1,11 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useAxiosSecure from './useAxiosSecure';
 
 const useAllUser = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const axiosSecure = useAxiosSecure()
+    const {data : users = [], refetch} = useQuery({
+        queryKey : ["users"],
+        queryFn : async () => {
+            const res = axiosSecure.get("/allUser")
+            return res.data
+        }
+    })
+    return [users, refetch]
 };
 
 export default useAllUser;
