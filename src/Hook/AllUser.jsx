c559b3e7dd0c66d57c67c128a2b955dd37dcaa0e
safinhr <hrsafin2434@gmail.com>
2024-01-28 20:26:48 +0600

    used react query

--- src/Hook/AllUser.jsx
+++ src/Hook/AllUser.jsx
@@ -1,10 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import useAxiosSecure from './useAxiosSecure';
+import { useContext } from 'react';
+import { AuthContext } from '../AuthProvider/AuthProvider';
 
 const AllUser = () => {
     const axiosSecure = useAxiosSecure()
+    const {user} = useContext(AuthContext)
     const {data : users = [], refetch} = useQuery({
-        queryKey : ["users"],
+        queryKey : ["users", user?.email],
         queryFn : async () => {
             const res = await axiosSecure.get("/allUser", {
                 headers : {
